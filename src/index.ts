02da2ad392c09a1e757335792e16c76a10680a8c
Wyatt Johnson <me@wyattjoh.ca>
2020-03-18 16:18:52 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import yargs from "yargs";
+import fs from "fs";
+import path from "path";
+
+const { version } = readJSON(path.join(__dirname, "..", "package.json"))!;
+
+const { argv } = yargs
+  .usage(
+    "package-version-lint: checks that your package.json and package-lock.json are in sync"
+  )
+  .help("help")
+  .alias("help", "h")
+  .version("version", version)
+  .alias("version", "v")
+  .options({
+    expect: {
+      type: "string",
+      description: "the version that the package.json should be checked against"
+    }
+  });
+
+interface Package {
+  version: string;
+}
+
+function readJSON(path: string) {
+  if (!fs.existsSync(path)) {
+    return null;
+  }
+
+  return JSON.parse(fs.readFileSync(path, "utf8")) as Package;
+}
+
+function compare() {
+  // Open the package.json file and parse it.
+  const pkg = readJSON("package.json");
+  if (!pkg) {
+    console.error(
+      "could not find package.json, are you running this from your package root?"
+    );
+    process.exit(1);
+  }
+
+  // Open the package-lock.json file and parse it.
+  const lock = readJSON("package-lock.json");
+  if (lock && pkg.version !== lock.version) {
+    console.error(
+      "package.json version does not match package-lock.json version"
+    );
+    process.exit(1);
+  }
+
+  if (argv.expect && pkg.version !== argv.expect) {
+    console.error(
+      `expected to find version ${argv.expect} but instead got ${pkg.version}`
+    );
+    process.exit(1);
+  }
+}
+
+compare();
